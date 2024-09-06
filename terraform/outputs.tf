c27d12d0ba369448ea38749b2a12dfea21c90975
lucy <Lucyouyang2023@gmail.com>
2024-09-06 09:39:21 +0900

    1

---
+++ terraform/outputs.tf
@@ -0,0 +1,12 @@
+# terraform/outputs.tf  
+output "ecr_repository_url" {
+  value = aws_ecr_repository.my_app.repository_url
+}
+
+output "subnet_id" {
+  value = aws_subnet.main.id
+}
+
+output "security_group_id" {
+  value = aws_security_group.ecs_sg.id
+}
