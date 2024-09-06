787f93cb0be1e3eedcb717d753b60e296ebecf58
lucy <Lucyouyang2023@gmail.com>
2024-09-06 13:11:36 +0900

    1

---
+++ terraform/main.tf
@@ -0,0 +1,117 @@
+provider "aws" {
+  region = "ap-northeast-1"  # 替换为您的区域
+}
+
+resource "aws_ecr_repository" "my_app" {
+  name = "my-app"
+}
+
+# resource "aws_ecs_cluster" "my_cluster" {
+#   name = "my-cluster"
+# }
+
+# resource "aws_vpc" "main" {
+#   cidr_block = "10.0.0.0/16"
+#   enable_dns_support = true
+#   enable_dns_hostnames = true
+#   tags = {
+#     Name = "main-vpc"
+#   }
+# }
+
+# resource "aws_subnet" "main" {
+#   vpc_id     = aws_vpc.main.id
+#   cidr_block = "10.0.1.0/24"
+#   availability_zone = "ap-northeast-1a"
+#   tags = {
+#     Name = "main-subnet"
+#   }
+# }
+
+# resource "aws_security_group" "ecs_sg" {
+#   name        = "ecs_sg"
+#   description = "Allow inbound traffic"
+#   vpc_id      = aws_vpc.main.id
+
+#   ingress {
+#     from_port   = 80
+#     to_port     = 80
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   ingress {
+#     from_port   = 22
+#     to_port     = 22
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#    ingress {
+#     from_port   = 5000
+#     to_port     = 5000
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
+
+# resource "aws_internet_gateway" "main" {
+#   vpc_id = aws_vpc.main.id
+
+#   tags = {
+#     Name = "main-gateway"
+#   }
+# }
+
+# resource "aws_route_table" "public_rt" {
+#   vpc_id = aws_vpc.main.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.main.id
+#   }
+# }
+
+# resource "aws_route_table_association" "public_rt_assoc" {
+#   subnet_id      = aws_subnet.main.id
+#   route_table_id = aws_route_table.public_rt.id
+# }
+
+# resource "aws_ecs_task_definition" "my_task" {
+#   family                   = "my-task"
+#   execution_role_arn   = "arn:aws:iam::822573796369:role/ecsTaskExecutionRole"
+#   network_mode             = "awsvpc"
+#   requires_compatibilities = ["FARGATE"]
+#   cpu                      = "256"
+#   memory                   = "512"
+
+#   container_definitions = jsonencode([{
+#     name      = "my-app"
+#     image     = "${aws_ecr_repository.my_app.repository_url}:latest"
+#     essential = true
+#     portMappings = [{
+#       containerPort = 5000
+#       hostPort      = 5000
+#     }]
+#   }])
+# }
+
+# resource "aws_ecs_service" "my_service" {
+#   name            = "my-service"
+#   cluster         = aws_ecs_cluster.my_cluster.id
+#   task_definition = aws_ecs_task_definition.my_task.arn
+#   desired_count   = 1
+#   launch_type     = "FARGATE"
+#   network_configuration {
+#     subnets         = [aws_subnet.main.id]
+#     security_groups = [aws_security_group.ecs_sg.id]
+#   }
+# }
+#1
