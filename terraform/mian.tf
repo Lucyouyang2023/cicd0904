9e3472cb36a42af0952ebf37367d6fa673b988a9
lucy <Lucyouyang2023@gmail.com>
2024-09-09 16:51:20 +0900

    1

--- terraform/mian.tf
+++ terraform/mian.tf
@@ -1,3 +1,18 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   enable_dns_support = true
