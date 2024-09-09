6d5414462b3ccc2022b544899dc8df89cff52967
lucy <Lucyouyang2023@gmail.com>
2024-09-09 17:21:15 +0900

    1

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,15 @@
+# output "subnet_id" {
+#   value = aws_subnet.main.id
+# }
+
+
 output "subnet_id" {
   value = aws_subnet.main.id
+}
+
+# 以不同的方式设置环境变量
+resource "null_resource" "set_env" {
+  provisioner "local-exec" {
+    command = "echo '::set-env name=SUBNET_ID::${aws_subnet.main.id}'"
+  }
 }
